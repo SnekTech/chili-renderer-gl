1b58d497d63e43ca73398f4e80b884df7d75fdfe
syf <2654525303@qq.com>
2023-03-15 18:17:19 +0800

    give cube faces different colors

--- src/Game.cpp
+++ src/Game.cpp
@@ -16,12 +16,12 @@ Game::Game()
 
 void Game::Go()
 {
-	gfx.BeginFrame();
+    gfx.BeginFrame();
 
     controller.UpdateState();
-	UpdateModel();
-	ComposeFrame();
-	gfx.EndFrame();
+    UpdateModel();
+    ComposeFrame();
+    gfx.EndFrame();
 }
 
 void Game::UpdateModel()
@@ -46,23 +46,41 @@ void Game::UpdateModel()
 
 void Game::ComposeFrame()
 {
+    const Color colors[12] = {
+        Colors::White,
+        Colors::Blue,
+        Colors::Cyan,
+        Colors::Gray,
+        Colors::Green,
+        Colors::Magenta,
+        Colors::LightGray,
+        Colors::Red,
+        Colors::Yellow,
+        Colors::White,
+        Colors::Blue,
+        Colors::Cyan
+    };
+
     Mat3 rot = Mat3::RotationX(theta_x)
-        * Mat3::RotationY(theta_y);
+               * Mat3::RotationY(theta_y);
 
     auto triangles = cube.GetTriangles();
 
-	for (auto& v : triangles.vertices)
-	{
+    for (auto& v: triangles.vertices)
+    {
         v *= rot;
-		v += {0, 0, offset_z};
-		pst.Transform(v);
-	}
-	for (auto i = triangles.indices.cbegin(); i != triangles.indices.cend(); std::advance(i, 3))
-	{
+        v += { 0, 0, offset_z };
+        pst.Transform(v);
+    }
+    for (auto i = triangles.indices.cbegin(); i != triangles.indices.cend(); std::advance(i, 3))
+    {
         using std::next;
+
         Vec2 v0 = triangles.vertices[*i];
         Vec2 v1 = triangles.vertices[*next(i)];
         Vec2 v2 = triangles.vertices[*next(i, 2)];
-        gfx.DrawTriangle(v0, v1, v2, Colors::White);
-	}
+
+        unsigned int colorIndex = std::distance(triangles.indices.cbegin(), i) / 3;
+        gfx.DrawTriangle(v0, v1, v2, colors[colorIndex]);
+    }
 }
