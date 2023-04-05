cee9110a3f6d806fcb4f4351b6a242a258067213
syf <2654525303@qq.com>
2023-04-05 18:20:00 +0800

    per-pixel phone shading

--- src/Game.cpp
+++ src/Game.cpp
@@ -16,12 +16,14 @@
 #include "Scenes/GeometryFlatScene.h"
 #include "Scenes/GouraudScene.h"
 #include "Scenes/GouraudPointScene.h"
+#include "Scenes/PhongPointScene.h"
 
 using Button = Widgets::Controller::Button;
 
 
 Game::Game(GLFWwindow* window) : controller(GLFW_JOYSTICK_1), keyboard(window)
 {
+    scenes.push_back(std::make_unique<PhongPointScene>(gfx, Plane::GetNormals<PhongPointScene::Vertex>(1)));
     scenes.push_back(std::make_unique<GouraudPointScene>(gfx, Plane::GetNormals<GouraudPointScene::Vertex>(1)));
     scenes.push_back(std::make_unique<GouraudPointScene>(gfx, IndexedTriangleList<GouraudPointScene::Vertex>::LoadNormals("../models/suzanne.obj")));
     scenes.push_back(std::make_unique<GouraudScene>(gfx, IndexedTriangleList<GouraudEffect::Vertex>::LoadNormals("../models/suzanne.obj")));
