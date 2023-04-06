978bbb64ff07132467df0f604609d073a17633ad
syf <2654525303@qq.com>
2023-04-06 17:13:49 +0800

    4d matrix & vector

--- src/DataStructures/Mat.h
+++ src/DataStructures/Mat.h
@@ -1,9 +1,7 @@
 //
 // Created by syf on 2023/3/10.
 //
-
-#ifndef CHILI_RENDERER_GL_MAT_H
-#define CHILI_RENDERER_GL_MAT_H
+#pragma once
 
 #include <string>
 #include <array>
@@ -12,98 +10,91 @@
 #include "Vec3.h"
 #include "Vec4.h"
 
-template <typename T, unsigned int Size>
-class Matrix
+template<typename T, unsigned int Size>
+class MatrixImpl
 {
 private:
-	using MatElements = std::array<std::array<T, Size>, Size>;
+    using MatElements = std::array<std::array<T, Size>, Size>;
 public:
-    Matrix() = default;
-	explicit Matrix(const MatElements& elements)
-	{
-		int i = 0;
-		for (auto& row : elements)
-		{
-			std::copy(row.begin(), row.end(), Elements[i].begin());
-			i++;
-		}
-	}
-	Matrix(const Matrix& other): Matrix(other.Elements)
-	{
-	}
+    MatrixImpl() = default;
 
-	void swap(Matrix& rhs) noexcept
-	{
-		using std::swap;
-		swap(Elements, rhs.Elements);
-	}
-	Matrix& operator=(Matrix rhs)
-	{
-		rhs.swap(*this);
-		return *this;
-	}
-	Matrix& operator*=(T rhs)
-	{
-		for (auto& row : Elements)
-		{
-			for (auto& e : row)
-			{
-				e *= rhs;
-			}
-		}
-		return *this;
-	}
-	Matrix operator*(T rhs) const
-	{
-		Matrix result = *this;
-		return result *= rhs;
-	}
-	Matrix& operator*=(const Matrix& rhs)
-	{
-		return *this = *this * rhs;
-	}
-	Matrix operator*(const Matrix& rhs) const
-	{
-		MatElements resultElements;
+    explicit MatrixImpl(const MatElements& elements)
+    {
+        int i = 0;
+        for (auto& row: elements)
+        {
+            std::copy(row.begin(), row.end(), Elements[i].begin());
+            i++;
+        }
+    }
 
-		for (int i = 0; i < Size; i++)
-		{
-			for (int j = 0; j < Size; j++)
-			{
-				T sum = 0;
-				for (int k = 0; k < Size; k++)
-				{
-					sum += Elements[i][k] * rhs.Elements[k][j];
-				}
-				resultElements[i][j] = sum;
-			}
-		}
+    MatrixImpl& operator*=(T rhs)
+    {
+        for (auto& row: Elements)
+        {
+            for (auto& e: row)
+            {
+                e *= rhs;
+            }
+        }
+        return *this;
+    }
 
-		return Matrix(resultElements);
-	}
-	Matrix operator!()
-	{
-		Matrix xp;
-		for (int i = 0; i < Size; i++)
-		{
-			for (int j = 0; j < Size; j++)
-			{
-				xp.Elements[i][j] = Elements[j][i];
-			}
-		}
-		return xp;
-	}
-	static Matrix Identity()
-	{
-		MatElements identity;
+    MatrixImpl operator*(T rhs) const
+    {
+        MatrixImpl result = *this;
+        return result *= rhs;
+    }
 
-		if constexpr(Size == 2)
-		{
-			identity = {
-				1, 0,
-				0, 1
-			};
-            return Matrix(identity);
+    MatrixImpl& operator*=(const MatrixImpl& rhs)
+    {
+        return *this = *this * rhs;
+    }
+
+    MatrixImpl operator*(const MatrixImpl& rhs) const
+    {
+        MatElements resultElements;
+
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                T sum = 0;
+                for (int k = 0; k < Size; k++)
+                {
+                    sum += Elements[i][k] * rhs.Elements[k][j];
+                }
+                resultElements[i][j] = sum;
+            }
+        }
+
+        return MatrixImpl(resultElements);
+    }
+
+    MatrixImpl operator!()
+    {
+        MatrixImpl xp;
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                xp.Elements[i][j] = Elements[j][i];
+            }
+        }
+        return xp;
+    }
+
+    static MatrixImpl Identity()
+    {
+        MatElements identity;
+
+        if constexpr (Size == 2)
+        {
+            identity = {
+                1, 0,
+                0, 1
+            };
+            return MatrixImpl(identity);
         }
         if constexpr (Size == 3)
         {
@@ -113,7 +104,7 @@ public:
                     0.0f, 1.0f, 0.0f,
                     0.0f, 0.0f, 1.0f
                 };
-            return Matrix(identity);
+            return MatrixImpl(identity);
         }
         if constexpr (Size == 4)
         {
@@ -123,44 +114,41 @@ public:
                 0, 0, 1, 0,
                 0, 0, 0, 1
             };
-            return Matrix(identity);
+            return MatrixImpl(identity);
         }
 
         throw std::runtime_error("bad matrix dimensionality");
-	}
-	static Matrix Scaling(T factor)
-	{
-		return Identity() * factor;
-	}
-	static Matrix RotationZ(T theta)
-	{
-		const T sinTheta = sin(theta);
-		const T cosTheta = cos(theta);
+    }
 
-		if constexpr (Size == 3)
-		{
-			MatElements raw =
-			{
-				cosTheta, sinTheta, 0,
-				-sinTheta, cosTheta, 0,
-				0, 0, 1
-			};
-			return Matrix(raw);
-		}
-		if constexpr (Size == 4)
-		{
-			MatElements  raw = {
-				cosTheta, sinTheta, 0, 0,
-               -sinTheta, sinTheta, 0, 0,
-				0, 			0, 		1, 0,
-				0, 			0, 		0, 1
-			};
-            return raw;
-		}
+    static MatrixImpl Scaling(T factor)
+    {
+        MatElements identity;
+
+        if constexpr (Size == 3)
+        {
+            identity =
+                {
+                    factor, 0.0f, 0.0f,
+                    0.0f, factor, 0.0f,
+                    0.0f, 0.0f, factor
+                };
+            return MatrixImpl(identity);
+        }
+        if constexpr (Size == 4)
+        {
+            identity = {
+                factor, 0.0f, 0.0f, 0.0f,
+                0.0f, factor, 0.0f, 0.0f,
+                0.0f, 0.0f, factor, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f
+            };
+            return MatrixImpl(identity);
+        }
 
         throw std::runtime_error("bad matrix dimensionality");
-	}
-    static Matrix RotationY(T theta)
+    }
+
+    static MatrixImpl RotationZ(T theta)
     {
         const T sinTheta = sin(theta);
         const T cosTheta = cos(theta);
@@ -169,27 +157,27 @@ public:
         {
             MatElements raw =
                 {
-                    cosTheta, 0, -sinTheta,
-                    0,        1, 0,
-                    sinTheta, 0, cosTheta
+                    cosTheta, sinTheta, 0.0f,
+                    -sinTheta, cosTheta, 0.0f,
+                    0.0f, 0.0f, 1.0f,
                 };
-            return Matrix(raw);
+            return MatrixImpl(raw);
         }
         if constexpr (Size == 4)
         {
-            MatElements  raw =
-                {
-                    cosTheta, 0, -sinTheta, 0,
-                    0,        1, 0,         0,
-                    sinTheta, 0, cosTheta,  0,
-                    0,        0, 0,         0
-                };
+            MatElements raw = {
+                cosTheta, sinTheta, 0.0f, 0.0f,
+                -sinTheta, cosTheta, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f, 0,
+                0.0f, 0.0f, 0.0f, 1.0f,
+            };
             return raw;
         }
 
         throw std::runtime_error("bad matrix dimensionality");
     }
-    static Matrix RotationX(T theta)
+
+    static MatrixImpl RotationY(T theta)
     {
         const T sinTheta = sin(theta);
         const T cosTheta = cos(theta);
@@ -197,76 +185,123 @@ public:
         if constexpr (Size == 3)
         {
             MatElements raw =
-            {
-                1, 0,           0,
-                0, cosTheta, sinTheta,
-                0, -sinTheta, cosTheta,
-            };
-            return Matrix(raw);
+                {
+                    cosTheta, 0.0f, -sinTheta,
+                    0.0f, 1.0f, 0.0f,
+                    sinTheta, 0.0f, cosTheta
+                };
+            return MatrixImpl(raw);
         }
         if constexpr (Size == 4)
         {
             MatElements raw =
-            {
-                1, 0,        0,         0,
-                0, cosTheta, sinTheta,  0,
-                0, -sinTheta, cosTheta, 0,
-                0, 0,        0,         1
-            };
-            return Matrix(raw);
+                {
+                    cosTheta, 0.0f, -sinTheta, 0.0f,
+                    0.0f, 1.0f, 0.0f, 0.0f,
+                    sinTheta, 0.0f, cosTheta, 0.0f,
+                    0.0f, 0.0f, 0.0f, 0.0f,
+                };
+            return raw;
         }
 
         throw std::runtime_error("bad matrix dimensionality");
     }
+
+    static MatrixImpl RotationX(T theta)
+    {
+        const T sinTheta = sin(theta);
+        const T cosTheta = cos(theta);
+
+        if constexpr (Size == 3)
+        {
+            MatElements raw =
+                {
+                    1.0f, 0.0f, 0.0f,
+                    0.0f, cosTheta, sinTheta,
+                    0.0f, -sinTheta, cosTheta,
+                };
+            return MatrixImpl(raw);
+        }
+        if constexpr (Size == 4)
+        {
+            MatElements raw =
+                {
+                    1.0f, 0.0f, 0.0f, 0.0f,
+                    0.0f, cosTheta, sinTheta, 0.0f,
+                    0.0f, -sinTheta, cosTheta, 0.0f,
+                    0.0f, 0.0f, 0.0f, 1.0f,
+                };
+            return MatrixImpl(raw);
+        }
+
+        throw std::runtime_error("bad matrix dimensionality");
+    }
+
+    template<class V>
+    static MatrixImpl Translation(const V& translation)
+    {
+        return Translation(translation.x, translation.y, translation.z);
+    }
+
+    static MatrixImpl Translation(T x, T y, T z)
+    {
+        if constexpr (Size == 4)
+            return MatrixImpl({
+                1.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f, 0.0f,
+                x, y, z, 1.0f,
+            });
+
+        throw std::runtime_error("bad matrix dimensionality");
+    }
+
 public:
-	MatElements Elements;
+    MatElements Elements;
 };
 
 template<typename T>
-Vector3<T>& operator *= (Vector3<T>& lhs, const Matrix<T, 3>& rhs)
+Vector3<T>& operator*=(Vector3<T>& lhs, const MatrixImpl<T, 3>& rhs)
 {
-	return lhs = lhs * rhs;
+    return lhs = lhs * rhs;
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector3<T>& lhs, const Matrix<T, 3>& rhs)
+Vector3<T> operator*(const Vector3<T>& lhs, const MatrixImpl<T, 3>& rhs)
 {
-	const auto& matrix = rhs.Elements;
-	return
-	{
-		lhs.x * matrix[0][0] + lhs.y * matrix[1][0] + lhs.z * matrix[2][0],
-		lhs.x * matrix[0][1] + lhs.y * matrix[1][1] + lhs.z * matrix[2][1],
-		lhs.x * matrix[0][2] + lhs.y * matrix[1][2] + lhs.z * matrix[2][2],
-	};
+    const auto& matrix = rhs.Elements;
+    return
+        {
+            lhs.x * matrix[0][0] + lhs.y * matrix[1][0] + lhs.z * matrix[2][0],
+            lhs.x * matrix[0][1] + lhs.y * matrix[1][1] + lhs.z * matrix[2][1],
+            lhs.x * matrix[0][2] + lhs.y * matrix[1][2] + lhs.z * matrix[2][2],
+        };
 }
 
 template<typename T>
-Vector3<T>& operator *= (Vector4<T>& lhs, const Matrix<T, 4>& rhs)
+Vector4<T>& operator*=(Vector4<T>& lhs, const MatrixImpl<T, 4>& rhs)
 {
-	return lhs = lhs * rhs;
+    return lhs = lhs * rhs;
 }
 
 template<typename T>
-Vector3<T> operator*(const Vector4<T>& lhs, const Matrix<T, 4>& rhs)
+Vector4<T> operator*(const Vector4<T>& lhs, const MatrixImpl<T, 4>& rhs)
 {
-	const auto& matrix = rhs.Elements;
-	return
-		{
-			lhs.x * matrix[0][0] + lhs.y * matrix[1][0] + lhs.z * matrix[2][0] + lhs.w * matrix[3][0],
-			lhs.x * matrix[0][1] + lhs.y * matrix[1][1] + lhs.z * matrix[2][1] + lhs.w * matrix[3][1],
-			lhs.x * matrix[0][2] + lhs.y * matrix[1][2] + lhs.z * matrix[2][2] + lhs.w * matrix[3][2],
-			lhs.x * matrix[0][3] + lhs.y * matrix[1][3] + lhs.z * matrix[2][3] + lhs.w * matrix[3][3],
-		};
+    const auto& matrix = rhs.Elements;
+    return
+        {
+            lhs.x * matrix[0][0] + lhs.y * matrix[1][0] + lhs.z * matrix[2][0] + lhs.w * matrix[3][0],
+            lhs.x * matrix[0][1] + lhs.y * matrix[1][1] + lhs.z * matrix[2][1] + lhs.w * matrix[3][1],
+            lhs.x * matrix[0][2] + lhs.y * matrix[1][2] + lhs.z * matrix[2][2] + lhs.w * matrix[3][2],
+            lhs.x * matrix[0][3] + lhs.y * matrix[1][3] + lhs.z * matrix[2][3] + lhs.w * matrix[3][3],
+        };
 }
 
-using Mat2 = Matrix<float, 2>;
-using Mad2 = Matrix<double, 2>;
+using Mat2 = MatrixImpl<float, 2>;
+using Mad2 = MatrixImpl<double, 2>;
 
-using Mat3 = Matrix<float, 3>;
-using Mad3 = Matrix<double, 3>;
+using Mat3 = MatrixImpl<float, 3>;
+using Mad3 = MatrixImpl<double, 3>;
 
-using Mat4 = Matrix<float, 4>;
-using Mad4 = Matrix<double, 4>;
-
-
-#endif //CHILI_RENDERER_GL_MAT_H
+using Mat4 = MatrixImpl<float, 4>;
+using Mad4 = MatrixImpl<double, 4>;
--- src/DataStructures/Vec4.h
+++ src/DataStructures/Vec4.h
@@ -24,163 +24,185 @@
 #include "../Widgets/ChiliMath.h"
 #include "Vec3.h"
 
-template <typename T>
+template<typename T>
 class Vector4
 {
 public:
-	Vector4() = default;
-	Vector4( T x,T y,T z,T w )
-		:
-		x(x), y(y), z(z), w( w )
-	{}
-	Vector4( const Vector3<T>& v3,float w = 1.0f  )
-		:
-		x(v3.x),y(v3.y),z(v3.z),
-		w( w )
-	{}
-	template <typename T2>
-	explicit operator Vector4<T2>() const
-	{
-		return{ (T2)x,(T2)y,(T2)z,(T2)w };
-	}
-	//T		LenSq() const
-	//{
-	//	return sq( *this );
-	//}
-	//T		Len() const
-	//{
-	//	return sqrt( LenSq() );
-	//}
-	//_Vec3&	Normalize()
-	//{
-	//	const T length = Len();
-	//	x /= length;
-	//	y /= length;
-	//	z /= length;
-	//	return *this;
-	//}
-	//_Vec3	GetNormalized() const
-	//{
-	//	_Vec3 norm = *this;
-	//	norm.Normalize();
-	//	return norm;
-	//}
-	Vector4	operator-() const
-	{
-		return Vector4( -x,-y,-z,-w );
-	}
-	Vector4&	operator=( const Vector4 &rhs )
-	{
-		x = rhs.x;
-		y = rhs.y;
-		z = rhs.z;
-		w = rhs.w;
-		return *this;
-	}
-	Vector4&	operator+=( const Vector4 &rhs )
-	{
-		x += rhs.x;
-		y += rhs.y;
-		z += rhs.z;
-		w += rhs.w;
-		return *this;
-	}
-	Vector4&	operator-=( const Vector4 &rhs )
-	{
-		x -= rhs.x;
-		y -= rhs.y;
-		z -= rhs.z;
-		w -= rhs.w;
-		return *this;
-	}
-	//T		operator*( const _Vec4 &rhs ) const
-	//{
-	//	return x * rhs.x + y * rhs.y + z * rhs.z;
-	//}
-	Vector4	operator+( const Vector4 &rhs ) const
-	{
-		return Vector4( *this ) += rhs;
-	}
-	Vector4	operator-( const Vector4 &rhs ) const
-	{
-		return Vector4( *this ) -= rhs;
-	}
-	Vector4&	operator*=( const T &rhs )
-	{
-		x *= rhs;
-		y *= rhs;
-		z *= rhs;
-		w *= rhs;
-		return *this;
-	}
-	Vector4	operator*( const T &rhs ) const
-	{
-		return Vector4( *this ) *= rhs;
-	}
-	//_Vec4	operator%( const _Vec4& rhs ) const
-	//{
-	//	return _Vec4(
-	//		y * rhs.z - z * rhs.y,
-	//		z * rhs.x - x * rhs.z,
-	//		x * rhs.y - y * rhs.x );
-	//}
-	Vector4&	operator/=( const T &rhs )
-	{
-		x /= rhs;
-		y /= rhs;
-		z /= rhs;
-		w /= rhs;
-		return *this;
-	}
-	Vector4	operator/( const T &rhs ) const
-	{
-		return Vector4( *this ) /= rhs;
-	}
-	bool	operator==( const Vector4 &rhs ) const
-	{
-		return x == rhs.x && y == rhs.y && rhs.z == z && rhs.w == w;
-	}
-	bool	operator!=( const Vector4 &rhs ) const
-	{
-		return !(*this == rhs);
-	}
-	// clamp to between 0.0 ~ 1.0
-	Vector4&	Saturate()
-	{
-		x = std::min( 1.0f,std::max( 0.0f,x ) );
-		y = std::min( 1.0f,std::max( 0.0f,y ) );
-		z = std::min( 1.0f,std::max( 0.0f,z ) );
-		w = std::min( 1.0f,std::max( 0.0f,w ) );
-		return *this;
-	}
-	// clamp to between 0.0 ~ 1.0
-	Vector4	GetSaturated() const
-	{
-		Vector4 temp( *this );
-		temp.Saturate();
-		return temp;
-	}
-	// x3 = x1 * x2 etc.
-	Vector4&  Hadamard( const Vector4& rhs )
-	{
-		x *= rhs.x;
-		y *= rhs.y;
-		z *= rhs.z;
-		w *= rhs.w;
-		return *this;
-	}
-	// x3 = x1 * x2 etc.
-	Vector4	GetHadamard( const Vector4& rhs ) const
-	{
-		Vector4 temp( *this );
-		temp.Hadamard( rhs );
-		return temp;
-	}
+    Vector4() = default;
+
+    Vector4(T x, T y, T z, T w)
+        :
+        x(x), y(y), z(z), w(w)
+    {
+    }
+
+    explicit Vector4(const Vector3<T>& v3, float w = 1.0f)
+        :
+        x(v3.x), y(v3.y), z(v3.z),
+        w(w)
+    {
+    }
+
+    template<typename T2>
+    explicit operator Vector4<T2>() const
+    {
+        return { (T2)x, (T2)y, (T2)z, (T2)w };
+    }
+
+    //T		LenSq() const
+    //{
+    //	return sq( *this );
+    //}
+    //T		Len() const
+    //{
+    //	return sqrt( LenSq() );
+    //}
+    //_Vec3&	Normalize()
+    //{
+    //	const T length = Len();
+    //	x /= length;
+    //	y /= length;
+    //	z /= length;
+    //	return *this;
+    //}
+    //_Vec3	GetNormalized() const
+    //{
+    //	_Vec3 norm = *this;
+    //	norm.Normalize();
+    //	return norm;
+    //}
+    Vector4 operator-() const
+    {
+        return Vector4(-x, -y, -z, -w);
+    }
+
+    Vector4& operator=(const Vector4& rhs)
+    {
+        x = rhs.x;
+        y = rhs.y;
+        z = rhs.z;
+        w = rhs.w;
+        return *this;
+    }
+
+    Vector4& operator+=(const Vector4& rhs)
+    {
+        x += rhs.x;
+        y += rhs.y;
+        z += rhs.z;
+        w += rhs.w;
+        return *this;
+    }
+
+    Vector4& operator-=(const Vector4& rhs)
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        z -= rhs.z;
+        w -= rhs.w;
+        return *this;
+    }
+
+    //T		operator*( const _Vec4 &rhs ) const
+    //{
+    //	return x * rhs.x + y * rhs.y + z * rhs.z;
+    //}
+    Vector4 operator+(const Vector4& rhs) const
+    {
+        return Vector4(*this) += rhs;
+    }
+
+    Vector4 operator-(const Vector4& rhs) const
+    {
+        return Vector4(*this) -= rhs;
+    }
+
+    Vector4& operator*=(const T& rhs)
+    {
+        x *= rhs;
+        y *= rhs;
+        z *= rhs;
+        w *= rhs;
+        return *this;
+    }
+
+    Vector4 operator*(const T& rhs) const
+    {
+        return Vector4(*this) *= rhs;
+    }
+
+    //_Vec4	operator%( const _Vec4& rhs ) const
+    //{
+    //	return _Vec4(
+    //		y * rhs.z - z * rhs.y,
+    //		z * rhs.x - x * rhs.z,
+    //		x * rhs.y - y * rhs.x );
+    //}
+    Vector4& operator/=(const T& rhs)
+    {
+        x /= rhs;
+        y /= rhs;
+        z /= rhs;
+        w /= rhs;
+        return *this;
+    }
+
+    Vector4 operator/(const T& rhs) const
+    {
+        return Vector4(*this) /= rhs;
+    }
+
+    bool operator==(const Vector4& rhs) const
+    {
+        return x == rhs.x && y == rhs.y && rhs.z == z && rhs.w == w;
+    }
+
+    bool operator!=(const Vector4& rhs) const
+    {
+        return !(*this == rhs);
+    }
+
+    // clamp to between 0.0 ~ 1.0
+    Vector4& Saturate()
+    {
+        x = std::min(1.0f, std::max(0.0f, x));
+        y = std::min(1.0f, std::max(0.0f, y));
+        z = std::min(1.0f, std::max(0.0f, z));
+        w = std::min(1.0f, std::max(0.0f, w));
+        return *this;
+    }
+
+    // clamp to between 0.0 ~ 1.0
+    Vector4 GetSaturated() const
+    {
+        Vector4 temp(*this);
+        temp.Saturate();
+        return temp;
+    }
+
+    // x3 = x1 * x2 etc.
+    Vector4& Hadamard(const Vector4& rhs)
+    {
+        x *= rhs.x;
+        y *= rhs.y;
+        z *= rhs.z;
+        w *= rhs.w;
+        return *this;
+    }
+
+    // x3 = x1 * x2 etc.
+    Vector4 GetHadamard(const Vector4& rhs) const
+    {
+        Vector4 temp(*this);
+        temp.Hadamard(rhs);
+        return temp;
+    }
+
 public:
     T x;
     T y;
     T z;
-	T w;
+    T w;
 };
 
 typedef Vector4<float> Vec4;
